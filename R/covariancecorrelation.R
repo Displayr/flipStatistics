29b6f54f0c899f20318e0824ed7750f8c28ff5b5
TimBock <tim.bock@numbers.net.au>
2016-10-05 17:11:39 +1100

    Refactoring Correlation

--- R/covariancecorrelation.R
+++ R/covariancecorrelation.R
@@ -1,61 +1,17 @@
 # Compute a matrix containing the pairwise weighted correlations between each column in
 # 'data'.
-weightedPartialCovarianceMatrix <- function(data, weight, correlation = FALSE)
+weightedPartialCovarianceMatrix <- function(data, weights, correlation = FALSE)
 {
-    .weightedPartialCovariance <- function(numeric1, numeric2, input.weight, correlation = FALSE)
-    {
-        Sumxy <- 0.0
-        SumY <- 0.0
-        SumX <- 0.0
-        SsX <- 0.0
-        SsY <- 0.0
-
-
-        # Vector Version
-
-        Wx <- input.weight * numeric1
-        Wy <- input.weight * numeric2
-        Wxy <- Wx * numeric2
-        Wxx <- Wx * numeric1
-        Wyy <- Wy * numeric2
-
-        complete.indicator <- !is.na(Wxy) & input.weight > 0
-
-        SumX <- sum(Wx[complete.indicator])
-        SumY <- sum(Wy[complete.indicator])
-        Sumxy <- sum(Wxy[complete.indicator])
-        SsX <- sum(Wxx[complete.indicator])
-        SsY <- sum(Wyy[complete.indicator])
-
-        # Calculate the Population for the
-        # weights for the pairwise complete observations
-        NWeighted <- sum(input.weight[complete.indicator])
-
-        # Covariance
-        Value <- Sumxy - SumX * SumY / NWeighted
-        if (correlation)
-        {
-            SdX <- sqrt(SsX - SumX * SumX / NWeighted)
-            SdY <- sqrt(SsY - SumY * SumY / NWeighted)
-            denominator <- SdX * SdY
-            return(Value / denominator)
-        } else {
-            return(Value / (NWeighted-1))
-        }
-    }
-
     num.cols <- ncol(data)
-    output.matrix <- matrix(0, nrow = num.cols, ncol = num.cols,
+    output.matrix <- matrix(1, nrow = num.cols, ncol = num.cols,
         dimnames = list(colnames(data), colnames(data)))
-    for (row in 1L:num.cols)
+    for (row in (1L + as.integer(correlation)):num.cols)
     {
-        for (col in row:num.cols)
+        for (col in 1:row)
         {
-            output.matrix[row, col] <- .weightedPartialCovariance(numeric1 = data[, row],
-                numeric2 = data[, col],
-                input.weight = weight,
-                correlation = correlation)
-            output.matrix[col, row] <- output.matrix[row, col]
+            output.matrix[row, col] <- Correlation(data[, row], data[, col], weights, correlation)
+            if (col != row)
+                output.matrix[col, row] <- output.matrix[row, col]
         }
     }
     return(output.matrix)
@@ -82,6 +38,7 @@ weightedPartialCovarianceMatrix <- function(data, weight, correlation = FALSE)
 #' my.data <- cbind(c(-0.9, 0.05, 0.1, 0.8), c(1, NaN, 0, -0.9))
 #' my.weight <- c(1.2, 0.8, 0.8, 1.2)
 #' CovarianceAndCorrelationMatrix(my.data, weights = my.weight, pairwise = TRUE)
+#' @importFrom stats cor
 #' @export
 CovarianceAndCorrelationMatrix <- function(data,
     weights = NULL,
@@ -90,25 +47,9 @@ CovarianceAndCorrelationMatrix <- function(data,
 {
     # Create the input correlation or convariance matrix
     if (is.null(weights))
-    {
-        # Unweighted options
-        if (pairwise)
-        {
-            use.string <- "pairwise.complete.obs"
-        }
-        else
-        {
-            use.string <- "complete.obs"
-        }
-
-        if (use.correlation)
-        {
-            input.matrix <- stats::cor(data, use = use.string)
-        }
-        else
-        {
-            input.matrix <- stats::cov(data, use = use.string)
-        }
+    {   # Unweighted options
+        use.string <- if (pairwise) "pairwise.complete.obs" else "complete.obs"
+        input.matrix <- if (use.correlation) cor(data, use = use.string) else cov(data, use = use.string)
     }
     else
     {
@@ -134,15 +75,47 @@ CovarianceAndCorrelationMatrix <- function(data,
 #' @param weights A numeric vector containing the value of the weight for each
 #'   row of \code{data}. If weights is NULL then this function is just a wrapper
 #'   for the base functions \code{cov} and \code{cor}.
+#' @param correlation If \code{FALSE}, returns the covariance.
 #' @examples
 #' x <- 1:10
 #' y <- c(2, 2:10)
 #' w <- rep(1, 10)
 #' Correlation(x, y, w)
 #' @export
-Correlation <- function(x, y, weights = NULL)
+Correlation <- function(x, y, weights = NULL, correlation = TRUE)
 {
-    CovarianceAndCorrelationMatrix(data.frame(x, y), weights, TRUE, TRUE)[2,1]
+    if (is.null(weights))
+        return(if (correlation) cor(x, y,  use = "complete.obs") else cov(x, y, use = "complete.obs"))
+    # Removing missing values.
+    valid <- !is.na(x) & !is.na(y) & !is.na(weights) & weights > 0
+    weights <- weights[valid]
+    x <- x[valid]
+    y <- y[valid]
+
+    Wx <- weights * x
+    Wy <- weights * y
+    Wxy <- Wx * y
+    Wxx <- Wx * x
+    Wyy <- Wy * y
+
+    SumX <- sum(Wx)
+    SumY <- sum(Wy)
+    Sumxy <- sum(Wxy)
+    SsX <- sum(Wxx)
+    SsY <- sum(Wyy)
+
+    # Calculate the Population for the
+    # weights for the pairwise complete observations
+    NWeighted <- sum(weights)
+
+    # Covariance
+    Value <- Sumxy - SumX * SumY / NWeighted
+    if (!correlation)
+        return(Value / (NWeighted - 1))
+    # Correlation.
+    SdX <- sqrt(SsX - SumX * SumX / NWeighted)
+    SdY <- sqrt(SsY - SumY * SumY / NWeighted)
+    return(Value / (SdX * SdY))
 }
 
 #' \code{CorrelationsWithSignificance}
