9523684e6c2192fc7cf28d8afd84b546a8d0d559
chschan <chschan@users.noreply.github.com>
2020-08-03 17:11:42 +1000

    Add tests

--- tests/testthat/test-table.R
+++ tests/testthat/test-table.R
@@ -7,9 +7,8 @@ test_that("Table", {
     df <- data.frame(val = 1:10,
             grp = factor(rep(letters[1:3], length = 10), levels = letters[1:4]))
 
-    # should 'd' be NaN or 0?
-    #expect_equal(as.vector(Table(val~grp, data = df, FUN = mean)),
-    #        c(a = 5.5, b = 5, c = 6, d = NA), check.attributes = FALSE)
+    expect_equal(as.vector(Table(val~grp, data = df, FUN = mean)),
+            c(a = 5.5, b = 5, c = 6, d = NA), check.attributes = FALSE)
 
     expect_equal(as.vector(Table(val~grp, data = df, FUN = sum)),
             c(a = 22, b = 15, c = 18, d = 0), check.attributes = FALSE)
