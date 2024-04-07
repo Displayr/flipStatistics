2fafff4a12bae617cd8a3069c209dab084d96826
Justin Yap <JustinCCYap@users.noreply.github.com>
2024-04-08 09:05:07 +1000

    Fix

--- tests/testthat/test-weightedtable.R
+++ tests/testthat/test-weightedtable.R
@@ -12,3 +12,20 @@ test_that("WeightedTable", {
     expect_equal(tab1/10, tab2)
     expect_equal(round(unname(tab3[,1])), c(423,635,224,353))
 })
+
+test_that("DS-5484", {
+    wgt = 1:2
+    x = c(NA, "A")
+    expected = structure(c(A = 2),
+                         .Dim = 1L,
+                         .Dimnames = list(x = "A"),
+                         class = "table")
+    expect_equal(WeightedTable(x, weights = wgt), expected)
+
+    expected = structure(c(2, 1),
+                         .Dim = 2L,
+                         .Dimnames = list(x = c("A", NA)),
+                         class = "table")
+    expect_equal(WeightedTable(x, weights = wgt, useNA = "ifany"), expected)
+})
+
