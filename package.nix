87a09fa6584edb0feabb22eaf5c1f2516d9264fd
Bruce Tan <bruce.tan@displayr.com>
2025-03-11 17:39:18 +1300

    DPI-2897 Pack flipStatistics in nix

---
+++ package.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {}, displayrUtils }:
+
+pkgs.rPackages.buildRPackage {
+  name = "flipStatistics";
+  version = displayrUtils.extractRVersion (builtins.readFile ./DESCRIPTION); 
+  src = ./.;
+  description = ''Computes standard statistics, dealing with situations not addressed
+    in base R. E.g., weighting, non-standard data structures.'';
+  propagatedBuildInputs = with pkgs.rPackages; [ 
+    flipTransformations
+    flipFormat
+    rhtmlHeatmap
+    verbs
+    flipU
+    survey
+  ];
+}
